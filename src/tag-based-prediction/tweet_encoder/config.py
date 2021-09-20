1308a206281e0491c2eb0be513c97ae21fe143e9
Xingyao Wang <xingyaow@umich.edu>
2021-09-20 17:57:25 -0400

    add code for tweet encoder

---
+++ src/tag-based-prediction/tweet_encoder/config.py
@@ -0,0 +1,27 @@
+import os
+
+class MultilabelConfig(object):
+    model = 'BERTweetModel'
+    dataset = 'GifReplyDataset'
+    multiclass = False # do multilabel classification
+    
+    dataset_path = "/home/xingyaow/gif-reply/data/processed/dataset/bertweet-normalize/finalized-split-dataset/tweet-gif-reply.pickle"
+    metadata_path = ""
+
+    load_model_path = None  # 'checkpoints/model.pth'
+
+    use_gpu = True  # use GPU or not
+    cuda_devices = [2]  # GPU device - if list then use multiple
+    num_workers = 8  # how many workers for loading data
+    batch_size = 32  # batch size
+    print_freq = 100  # print info every N batch
+
+    max_epoch = 100
+    lr = 1e-5  # initial learning rate
+    weight_decay = 1e-3 
+    random_seed = 42
+
+
+opt = MultilabelConfig()
+if opt.use_gpu:
+    os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(map(str, opt.cuda_devices))
---
+++ src/tag-based-prediction/tweet_encoder/data/__init__.py
@@ -0,0 +1,32 @@
+import torch
+from .dataset import (
+    GifReplyDataset
+)
+from torch.nn.utils.rnn import pad_sequence
+
+def pad_batch_sequence(X):
+    """ collate_fn of DataLoader.
+    Pad list of ((input_ids, label), ...) to the same length (max length in this batch).
+
+     - with automatic batching:
+        for indices in batch_sampler:
+            yield collate_fn([dataset[i] for i in indices])
+    """
+
+    input_ids, labels = list(zip(*X))
+    # input_ids -> list of torch.Tensor with not necessary the same shape
+    # labels -> list of torch.Tensor with same shape
+    input_ids = pad_sequence(input_ids, batch_first=True)
+    labels = torch.stack(labels)
+    return input_ids, labels
+
+COLLATE_FUNC_MAP = {
+    'BERTweetModel': pad_batch_sequence
+}
+
+
+def get_collate_fn(model_name):
+    if model_name not in COLLATE_FUNC_MAP:
+        return None
+    else:
+        return COLLATE_FUNC_MAP[model_name]
